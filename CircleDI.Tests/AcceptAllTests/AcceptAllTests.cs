9366314937000d7182176dff9fe0d9df0b7aef48
BlackWhiteYoshi <github@firerocket.de>
2025-11-11 21:34:16 +0100

    changed AcceptAllTests.py to AcceptAllTests.cs

---
+++ CircleDI.Tests/AcceptAllTests/AcceptAllTests.cs
@@ -0,0 +1,21 @@
+﻿#!/usr/bin/env dotnet
+
+/**
+ * goes throgh the folder and subfolders
+ *   if file name is "*.received.txt"
+ *     rename file to "*.verified.txt" (and overwrite existing file if any)
+ **/
+
+string currentDirectory = Directory.GetCurrentDirectory();
+AcceptFolder(Path.Join(currentDirectory, "..", "GeneratorTests"));
+AcceptFolder(Path.Join(currentDirectory, "..", "BlazorTests"));
+AcceptFolder(Path.Join(currentDirectory, "..", "MinimalAPITests"));
+
+void AcceptFolder(string directory) {
+    foreach (string fileName in Directory.GetFiles(directory, "*", SearchOption.AllDirectories))
+        if (fileName.EndsWith(".received.txt")) {
+            string baseName = fileName[..^".received.txt".Length];
+            File.Move(fileName, $"{baseName}.verified.txt", overwrite: true);
+            Console.WriteLine($"accepted {Path.GetFileName(baseName)}");
+        }
+}
